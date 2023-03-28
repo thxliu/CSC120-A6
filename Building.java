34be9886aadcaacf8b26054610b8f73099fb27c5
Tana <thliu@smith.edu>
2023-03-28 17:41:19 -0400

    commit

--- Building.java
+++ Building.java
@@ -1,9 +1,18 @@
+/**
+ * public class that creates a generic Building
+ */
 public class Building {
 
     protected String name = "<Name Unknown>";
     protected String address = "<Address Unknown>";
     protected int nFloors = 1;
 
+    /**
+     * Constructor to build a building
+     * @param name Building name
+     * @param address Building address
+     * @param nFloors Number of floors in the building
+     */
     public Building(String name, String address, int nFloors) {
         if (name != null) { this.name = name; }
         if (address != null) { this.address = address; } 
@@ -13,18 +22,33 @@ public class Building {
         this.nFloors = nFloors;
     }
 
+    /**
+     * accessor for the protected attribute name 
+     * @return the name of the building as a string
+     */
     public String getName() {
         return this.name;
     }
 
+    /**
+     * accessor for the protected attribute address
+     * @return the address of the building as a string
+     */
     public String getAddress() {
         return this.address;
     }
 
+    /**
+     * accessor for the protected attribute nFloors
+     * @return the number of floors in the building as an integer 
+     */
     public int getFloors() {
         return this.nFloors;
     }
 
+    /**
+     * Overrides the toString() method to give a formatted description of the building
+     */
     public String toString() {
         return this.name + " is a " + this.nFloors + "-story building located at " + this.address;
     }
--- reflection.md
+++ reflection.md
@@ -1,4 +1,7 @@
 Use this file to record your reflection on this assignment.
 
 - What are your initial impressions of how `Inheritance` affects your programming?
+I feel like it makes it convenient to already have access to public methods within a parent class so that we don't have to constantly rewrite them with each new class we create. Getting used to using super() took a moment, but overall I feel more comfortable using it. 
+
 - What worked, what didn't, what advice would you give someone taking this course in the future?
+I feel that overall, my code for each class works. I think that the restock method in the Cafe class could be more specific as restocking every single item, even when an item doesn't need it, is excessive. My advice is to carefully study the hashtable documentation because although it shares similar methods to ArrayLists like get() and contains(), there are specific ones for keys only, values only, or both. Additionally, working through that made figuring out the for loop for printCollection() more difficult that looping through an ArrayList.
